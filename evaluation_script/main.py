327d830979e02acb2a17f7a1de81816c450b5469
Kadek Rizky Fransisca Putra <rizky.royal@gmail.com>
2022-09-27 11:18:58 +0700

    Edit challenge configuration

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,81 +1,84 @@
-import random
+import sys
+import traceback
 
 
-def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
-    print("Starting Evaluation.....")
-    """
-    Evaluates the submission for a particular challenge phase and returns score
-    Arguments:
+def prepareUsersAnnotation(file):
+    with open(file, "r") as f:
+        text = f.readlines()
+    res = []
 
-        `test_annotations_file`: Path to test_annotation_file on the server
-        `user_submission_file`: Path to file submitted by the user
-        `phase_codename`: Phase to which submission is made
+    for line in text:
+            line = line.rstrip('\n') # remove \n at the end of line
+            items = line.split(',')
+            # change array/list to tuple
+            items[1] = items[1].split(' ')
+            res.append(tuple(items))
+    return res
 
-        `**kwargs`: keyword arguments that contains additional submission
-        metadata that challenge hosts can use to send slack notification.
-        You can access the submission metadata
-        with kwargs['submission_metadata']
+def prepareTestsAnnotation(file):
+    with open(file, "r") as f:
+        text = f.readlines()
+    res = dict()
 
-        Example: A sample submission metadata can be accessed like this:
-        >>> print(kwargs['submission_metadata'])
-        {
-            'status': u'running',
-            'when_made_public': None,
-            'participant_team': 5,
-            'input_file': 'https://abc.xyz/path/to/submission/file.json',
-            'execution_time': u'123',
-            'publication_url': u'ABC',
-            'challenge_phase': 1,
-            'created_by': u'ABC',
-            'stdout_file': 'https://abc.xyz/path/to/stdout/file.json',
-            'method_name': u'Test',
-            'stderr_file': 'https://abc.xyz/path/to/stderr/file.json',
-            'participant_team_name': u'Test Team',
-            'project_url': u'http://foo.bar',
-            'method_description': u'ABC',
-            'is_public': False,
-            'submission_result_file': 'https://abc.xyz/path/result/file.json',
-            'id': 123,
-            'submitted_at': u'2017-03-20T19:22:03.880652Z'
-        }
-    """
+    for line in text:
+            line = line.rstrip('\n') # remove \n at the end of line
+            items = line.split(',')
+            res[items[0]] = items[1].split(' ')
+    return res
+
+
+def evaluate(test_annotation_file ,user_annotation_file, phase_codename, **kwargs):
+
+    tests = prepareTestsAnnotation(test_annotation_file)
+    users = prepareUsersAnnotation(user_annotation_file)
+
+    # Delete Header
+    tests.pop('User')
+    users.pop(0)
+
+    numberOfUserAnnotationRows = len(users)
+    averagePrecision = 0
+    meanAveragePrecision = 0
+    for u in range(1,numberOfUserAnnotationRows + 1):
+
+        if users[u-1][0] not in tests:
+            continue
+
+        m = len(tests.get(users[u - 1][0]))
+        n = len(users[u - 1][1])
+        precisionAtK = 0
+
+        for k in range(1, min(n, 200) + 1):
+            relevant = 0
+            kth = False
+            for i in range(k):
+                if users[u - 1][1][i] in tests.get(users[u - 1][0]):
+                    relevant+=1
+                
+                if users[u - 1][1][i] in tests.get(users[u - 1][0]) and i == k-1:
+                    kth=True
+
+            if kth == True:
+                precisionAtK = precisionAtK + (relevant / k)
+
+        averagePrecision = averagePrecision + (precisionAtK / min(m, 200))
+
+    meanAveragePrecision = averagePrecision / max(len(tests), numberOfUserAnnotationRows)
+
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+
+    if phase_codename == "final":
+        try:
+            output['result'] = [
+                {
+                    'test_split': { # dataset split codename
+                        'Score': meanAveragePrecision,
+                    }
                 }
-            }
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]
-        print("Completed evaluation for Test Phase")
-    return output
+            ]
+            return output
+        except Exception as e:
+            sys.stderr.write(traceback.format_exc())
+            return e
+
+# print(evaluate("annotation.csv", "user_annotation.csv", phase_codename="final"))
